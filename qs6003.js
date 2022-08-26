c373a86e512d2fe414ff2d577833510df6b32d25
Vick Scarlet <scarlet_vick@outlook.com>
2022-08-26 21:00:17 +0800

    fix: 6003 judge

--- qs6003.js
+++ qs6003.js
@@ -4,10 +4,10 @@ export const question = '第六关：\n请选择一项身份。';
 //      -[special 表示特殊，配合 rate 使用万分率]
 export const options = {
     //                   score: 分数 extra: 额外分数
-    A: {type: 'usually', score: 4,           val: '国王（+4分，如果被刺杀，则这4分会被刺客平分）'},
-    B: {type: 'usually',                     val: '刺客（比卫兵多就刺杀国王）'},
-    C: {type: 'usually', score: 1, extra: 2, val: '卫兵（+1分，如果有活着的国王，额外+2分）'},
-    D: {type: 'usually', score: 1, extra: 1, val: '仆人（+1分，如果没有活着的国王，额外+1分）'},
+    A: {type: 'usually', score: 4,            val: '国王（+4分，如果被刺杀，则这4分会被刺客平分）'},
+    B: {type: 'usually',                      val: '刺客（比卫兵多就刺杀国王）'},
+    C: {type: 'usually', score: 1, extra:  2, val: '卫兵（+1分，如果有活着的国王，额外+2分）'},
+    D: {type: 'usually', score: 2, extra: -1, val: '仆人（+1分，如果没有活着的国王，额外+1分）'},
 };
 // 没有选的人的分数
 export const least = 0;
